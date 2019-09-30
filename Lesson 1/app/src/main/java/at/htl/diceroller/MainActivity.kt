41c69e5c5cfa42123d4511f8fc3fa75e107a34f0
Philipp323 <philipp-auinger@gmx.at>
2019-09-30 16:34:33 +0200

    Step 18: Exercise: Change the Text

--- Lesson 1/app/src/main/java/at/htl/diceroller/MainActivity.kt
+++ Lesson 1/app/src/main/java/at/htl/diceroller/MainActivity.kt
@@ -3,7 +3,9 @@ package at.htl.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,9 +13,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.roll_button)
-        rollButton.text = "Let's Roll"
         rollButton.setOnClickListener {
-            Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+            rollDice()
         }
+    }
+
+    private fun rollDice() {
+        val resultText: TextView = findViewById(R.id.result_text)
+
+        val randomInt = Random().nextInt(6) + 1
+        resultText.text = randomInt.toString()
+
     }
 }
