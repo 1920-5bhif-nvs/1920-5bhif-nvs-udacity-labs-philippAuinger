1d8e356d69548bd01c0159ec2c3c0d288ede9fe4
Philipp323 <philipp-auinger@gmx.at>
2020-01-05 11:52:57 +0100

    Lesson 7: Step 4: SleepNightAdapter implemented.

--- Lesson_6/andfun-kotlin-sleep-tracker-starter-code/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ Lesson_6/andfun-kotlin-sleep-tracker-starter-code/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -1,2 +1,18 @@
 package com.example.android.trackmysleepquality.sleeptracker
 
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.android.trackmysleepquality.database.SleepNight
+
+class SleepNightAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+    var data = listOf<SleepNight>()
+
+    override fun getItemCount() = data.size
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val item = data[position]
+        
+
+    }
+}
+
