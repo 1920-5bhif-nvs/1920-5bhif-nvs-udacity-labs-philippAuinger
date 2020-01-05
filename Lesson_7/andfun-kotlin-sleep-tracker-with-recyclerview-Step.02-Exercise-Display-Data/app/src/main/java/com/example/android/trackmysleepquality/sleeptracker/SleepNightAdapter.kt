65aa8dc56235381f407896923cffb019d855eca0
Philipp323 <philipp-auinger@gmx.at>
2020-01-05 12:39:07 +0100

    Lesson 7: Step 6: else BLACK Color added

--- Lesson_7/andfun-kotlin-sleep-tracker-with-recyclerview-Step.02-Exercise-Display-Data/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ Lesson_7/andfun-kotlin-sleep-tracker-with-recyclerview-Step.02-Exercise-Display-Data/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -39,6 +39,8 @@ class SleepNightAdapter: RecyclerView.Adapter<TextItemViewHolder>() {
         val item = data[position]
         if(item.sleepQuality <= 1){
             holder.textView.setTextColor(Color.RED)
+        } else {
+            holder.textView.setTextColor(Color.BLACK)
         }
         holder.textView.text = item.sleepQuality.toString()
     }
