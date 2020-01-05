09d78a53e692bab106c634ffa0aa24deca7a54f9
Philipp323 <philipp-auinger@gmx.at>
2020-01-05 12:33:46 +0100

    Lesson 7: Step 5: Observer in Fragment added

--- Lesson_7/andfun-kotlin-sleep-tracker-with-recyclerview-Step.05-Solution-Refactor-onBindViewHolder/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ Lesson_7/andfun-kotlin-sleep-tracker-with-recyclerview-Step.05-Solution-Refactor-onBindViewHolder/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2018, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.example.android.trackmysleepquality.sleeptracker
 
 import android.view.LayoutInflater
@@ -5,9 +21,9 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.android.trackmysleepquality.R
+import com.example.android.trackmysleepquality.convertDurationToFormatted
 import com.example.android.trackmysleepquality.convertNumericQualityToString
 import com.example.android.trackmysleepquality.database.SleepNight
 
@@ -53,8 +69,9 @@ class SleepNightAdapter : RecyclerView.Adapter<SleepNightAdapter.ViewHolder>() {
                 3 -> R.drawable.ic_sleep_3
                 4 -> R.drawable.ic_sleep_4
                 5 -> R.drawable.ic_sleep_5
-                else -> R.drawable.ic_sleep_5
+                else -> R.drawable.ic_sleep_active
             })
         }
     }
+
 }
